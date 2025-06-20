e15bab9195098382cad7cb3a069d5abd8feb0da3
john maina <maina202120@gmail.com>
2025-06-20 20:53:35 +0300

    compelete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare;
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance <= 2000) {
+        fare = (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+        fare = 25;
+    } else {
+        fare = 'cannot travel that far';
+    }
+    return fare;
+}
